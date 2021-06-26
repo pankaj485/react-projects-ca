b7cb0cf1306092f5573c589f4650be28123748be
Pankaj Thapaliya <61234787+pankaj485@users.noreply.github.com>
2021-06-26 14:25:38 +0530

    added initial files

---
+++ color-platte-generator/src/SingleColor.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import rgbToHex from "./utils";
+
+const SingleColor = ({ rgb, weight, index }) => {
+	const [alert, setAlert] = useState(false);
+	const bcg = rgb.join(",");
+	const hex = rgbToHex(...rgb);
+
+	useEffect(() => {
+		const timeOut = setTimeout(() => {
+			setAlert(false);
+		}, 2000);
+		return () => clearTimeout(timeOut);
+	}, [alert]);
+
+	return (
+		<article
+			className={`color ${index > 10 && "color-light"} colorContainer`}
+			style={{ backgroundColor: `rgb(${bcg})` }}
+		>
+			<p className="percent-value">{weight}</p>
+			<p className="color-value">{hex}</p>
+			{/* {alert && <p className="alert"> copied to clipboard </p>} */}
+			<button
+				className="btn copyBtn"
+				onClick={() => {
+					setAlert(true);
+					navigator.clipboard.writeText(hex);
+				}}
+			>
+				COPY
+			</button>
+		</article>
+	);
+};
+
+export default SingleColor;
