69d7d43400a83546b82fb37a24c22e234022600a
Pankaj Thapaliya <61234787+pankaj485@users.noreply.github.com>
2021-06-29 14:26:47 +0530

    added grocery bud files

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-## visit websites here 
+## visit websites here
 
 [color platte generator ](https://color-platte-generator.netlify.app/)
 
 [lorem-ipsum generator ](https://pank-lr-generate.netlify.app/)
+
+[Grocery Bud ](https://grocery-bud-pank.netlify.app/https://pank-lr-generate.netlify.app/)
---
+++ grocery-bud/src/List.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
+const List = ({ items, handleDelete, handleEdit }) => {
+	return (
+		<div className="grocery-list">
+			{items.map((item) => {
+				const { id, title } = item;
+				return (
+					<article className="grocery-item" key={id}>
+						<p className="title"> {title} </p>
+						<div className="btn-container">
+							<button
+								type="button"
+								className="edit-btn"
+								onClick={() => {
+									handleEdit(id);
+								}}
+							>
+								<FaEdit />
+							</button>
+							<button
+								type="button"
+								className="delete-btn"
+								onClick={() => {
+									handleDelete(id);
+								}}
+							>
+								<FaTrash />
+							</button>
+						</div>
+					</article>
+				);
+			})}
+		</div>
+	);
+};
+
+export default List;
